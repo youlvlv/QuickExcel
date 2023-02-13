d2c575eb60998efe8a4e51c78a7d9635e6cd834a
lizhiwei <10272523@qq.com>
2023-02-13 10:57:52 +0800

    更新注释

--- src/main/java/com/lizhiwei/quickExcel/core/ExcelUtil.java
+++ src/main/java/com/lizhiwei/quickExcel/core/ExcelUtil.java
@@ -204,7 +204,7 @@ public class ExcelUtil {
                         sheet.addMergedRegion(new CellRangeAddress(start, sheet.getRowNum() - 1, i, i));
                     }
                 }
-            } catch (IllegalAccessException | NoSuchFieldException e) {
+            } catch (Exception e) {
                 throw new ExcelValueError(e);
             }
         }
