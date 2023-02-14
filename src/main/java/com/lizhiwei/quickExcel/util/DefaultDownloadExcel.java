02fae7f62e944455a0a6ce0bd91aec417fc88eac
lizhiwei <10272523@qq.com>
2023-02-14 09:28:36 +0800

    优化Stream遇到异常未关闭的问题

--- src/main/java/com/lizhiwei/quickExcel/util/DefaultDownloadExcel.java
+++ src/main/java/com/lizhiwei/quickExcel/util/DefaultDownloadExcel.java
@@ -35,8 +35,7 @@ class DefaultDownloadExcel implements FileOperation {
         }
         String fileName = df.format(new Date()) + "-" + fileNameParam + ".xlsx";
         String fileName2 = "cache/" + fileName;
-        try {
-            FileOutputStream outFile = new FileOutputStream(fileName2);
+        try( FileOutputStream outFile = new FileOutputStream(fileName2);) {
             excel.write(outFile);
         } catch (IOException e) {
             throw new IORunTimeException(e);
@@ -45,25 +44,22 @@ class DefaultDownloadExcel implements FileOperation {
     }
 
     private static void downloadTemplate(HttpServletResponse response, String path) {
-        try {
-            String fileName = path.substring(path.lastIndexOf("/") + 1);
-            File file = new File(path);
+        String fileName = path.substring(path.lastIndexOf("/") + 1);
+        File file = new File(path);
+        try( BufferedInputStream inputStream = new BufferedInputStream(Files.newInputStream(file.toPath()));
+             OutputStream outputStream = response.getOutputStream();) {
             //作用：在前端作用显示为调用浏览器下载弹窗
             response.setHeader("Content-disposition", "attachment;filename=" + URLEncoder.encode(fileName, "UTF-8"));
             response.setContentType("application/octet-stream");
-            BufferedInputStream inputStream = new BufferedInputStream(Files.newInputStream(file.toPath()));
-            OutputStream outputStream = response.getOutputStream();
+
             byte[] buffer = new byte[1024];
             int len;
             while ((len = inputStream.read(buffer)) != -1) { /** 将流中内容写出去 .*/
                 outputStream.write(buffer, 0, len);
             }
-            inputStream.close();
-            outputStream.close();
         } catch (IOException e) {
             throw new IORunTimeException("文件操作失败", e);
         } finally {
-            File file = new File(path);
             file.delete();
         }
     }
