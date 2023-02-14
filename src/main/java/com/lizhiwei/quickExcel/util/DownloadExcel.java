73c1a05b3d927cee01882b499741a8358553bf8d
lizhiwei <10272523@qq.com>
2023-02-14 09:25:46 +0800

    优化Stream遇到异常未关闭的问题

--- src/main/java/com/lizhiwei/quickExcel/util/DownloadExcel.java
+++ src/main/java/com/lizhiwei/quickExcel/util/DownloadExcel.java
@@ -23,23 +23,19 @@ public class DownloadExcel extends ExcelUtil {
      * @throws IOException 异常
      */
     public static void downloadTemplate(HttpServletResponse response, String path) {
-        try {
 
-            String fileName = path.substring(path.lastIndexOf("/") + 1);
-            File file = new File(path);
+        String fileName = path.substring(path.lastIndexOf("/") + 1);
+        File file = new File(path);
+        try (OutputStream outputStream = response.getOutputStream(); BufferedInputStream inputStream = new BufferedInputStream(new FileInputStream(file));) {
             //作用：在前端作用显示为调用浏览器下载弹窗
             response.setHeader("Content-disposition", "attachment;filename=" + URLEncoder.encode(fileName, "UTF-8"));
             /*response.setHeader("Content-disposition", "attachment; filename = " + new String(fileName.getBytes(fileName), "ISO8859-1"));*/
             response.setContentType("application/octet-stream");
-            BufferedInputStream inputStream = new BufferedInputStream(new FileInputStream(file));
-            OutputStream outputStream = response.getOutputStream();
             byte[] buffer = new byte[1024];
             int len;
             while ((len = inputStream.read(buffer)) != -1) { /** 将流中内容写出去 .*/
                 outputStream.write(buffer, 0, len);
             }
-            inputStream.close();
-            outputStream.close();
         } catch (IOException e) {
             throw new IORunTimeException("文件操作失败", e);
         }
