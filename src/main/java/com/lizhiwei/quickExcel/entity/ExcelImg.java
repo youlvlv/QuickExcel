f147abb2a253c5316a745b153eede9ed40cedc0c
mpj <482395340@qq.com>
2024-03-06 18:38:50 +0800

    导入加图片

---
+++ src/main/java/com/lizhiwei/quickExcel/entity/ExcelImg.java
@@ -0,0 +1,59 @@
+package com.lizhiwei.quickExcel.entity;
+
+public class ExcelImg {
+
+	/**
+	 * noNum
+	 */
+	String noNum;
+
+
+	/**
+	 * 路径
+	 */
+	String path;
+
+	/**
+	 * 行
+	 */
+	String row;
+
+
+	/**
+	 * 列
+	 */
+	String col;
+
+
+	public String getNoNum() {
+		return noNum;
+	}
+
+	public void setNoNum(String noNum) {
+		this.noNum = noNum;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+
+	public String getRow() {
+		return row;
+	}
+
+	public void setRow(String row) {
+		this.row = row;
+	}
+
+	public String getCol() {
+		return col;
+	}
+
+	public void setCol(String col) {
+		this.col = col;
+	}
+}
