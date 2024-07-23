54272e3c2e5dcf219e11c7dce3589429a20f3225
lizhiwei <1027252304@qq.com>
2024-07-23 08:55:28 +0800

    refactor(quickExcel): convert UploadFile to functional interface
    Change UploadFile from an abstract class to a functional interface to
    improve code succinctness and leverage lambda expressions. This update
    allows for a cleaner implementation of the getFile method.

--- src/main/java/com/lizhiwei/quickExcel/model/UploadFile.java
+++ src/main/java/com/lizhiwei/quickExcel/model/UploadFile.java
@@ -2,24 +2,15 @@ package com.lizhiwei.quickExcel.model;
 
 
 import java.io.File;
-
 /**
  * 上传文件
  */
-public abstract class UploadFile {
-
-    private File file;
-
-
-    protected UploadFile(File file) {
-        this.file = file;
-    }
+@FunctionalInterface
+public interface UploadFile {
 
     /**
      * 获取文件
      * @return
      */
-    public File getFile() {
-        return file;
-    }
+    File getFile();
 }
